fdc51fbb96b7656fa87e665292a235fa07a555d0
NikitaPopovsky <popovsky.n@mail.ru>
2025-05-08 17:20:08 +0300

    Отладка

--- src/main/java/org/example/MainMergeTwoSortedLists.java
+++ src/main/java/org/example/MainMergeTwoSortedLists.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Main {
+public class MainMergeTwoSortedLists {
     public static void main(String[] args) {
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
