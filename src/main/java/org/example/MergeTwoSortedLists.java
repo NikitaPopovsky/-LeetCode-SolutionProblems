6d2b9c447f08291d13030f7cb9b9c2e168b6e388
NikitaPopovsky <popovsky.n@mail.ru>
2025-05-08 18:36:09 +0300

    Реализация аккуратного но долгого решения

--- src/main/java/org/example/MergeTwoSortedLists.java
+++ src/main/java/org/example/MergeTwoSortedLists.java
@@ -1,9 +1,50 @@
 package org.example;
 
+import java.util.*;
+
 public class MergeTwoSortedLists {
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //Аккуратное но долгое решение
+        if (list1 == null && list2 == null) {
+            return null;
+        }
+        ArrayList<Integer> generalList = new ArrayList<>();
 
-        ListNode ResultList = new ListNode();
-        return ResultList;
+        addToGeneralList(generalList, list1);
+        addToGeneralList(generalList, list2);
+
+        Collections.sort(generalList);
+
+        return createResultListNide (generalList);
+    }
+
+    private static void addToGeneralList (ArrayList<Integer> generalList, ListNode node) {
+        if (node == null) {
+            return;
+        }
+        boolean nextNotEmpty = true;
+        ListNode curNode = node;
+
+        while (nextNotEmpty) {
+            generalList.add(curNode.val);
+            curNode = curNode.next;
+            nextNotEmpty = curNode != null;
+        }
+
+    }
+
+    private static ListNode createResultListNide (ArrayList<Integer> generalList) {
+        ListNode resultNode = new ListNode();
+        ListNode curNode = resultNode;
+        int size = generalList.size() - 1;
+        for (int i = 0; i <= size ; i++ ) {
+            curNode.val = generalList.get(i);
+            if (i != size) {
+                curNode.next = new ListNode();
+                curNode = curNode.next;
+            }
+        }
+
+        return resultNode;
     }
 }
