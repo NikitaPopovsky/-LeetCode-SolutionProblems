300e4dcd698b1459eaef798922bea9b2c1694c45
NikitaPopovsky <popovsky.n@mail.ru>
2025-05-11 13:11:49 +0300

    Реализация решения задачи : 1550. Three Consecutive Odds https://leetcode.com/problems/three-consecutive-odds/description/?envType=daily-question&envId=2025-05-11

---
+++ src/main/java/MainThreeConsecutiveOdds_1550.java
@@ -0,0 +1,8 @@
+
+public class MainThreeConsecutiveOdds_1550 {
+    public static void main(String[] args) {
+        int [] arr = {1,2,34,3,4,5,7,23,12};
+        System.out.println(ThreeConsecutiveOdds_1550.threeConsecutiveOdds(arr));
+    }
+
+}
